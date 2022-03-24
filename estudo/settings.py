c091389367b7e841fd29ea1f324bd29a211f3e92
Victor Augusto <victor.soares@irricontrol.com.br>
2022-03-24 10:52:31 -0300

    added num_queries()

--- estudo/settings.py
+++ estudo/settings.py
@@ -118,3 +118,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# shell_plus - features
+SHELL_PLUS_PRE_IMPORTS = [('estudo.extra_imports', '*')]
